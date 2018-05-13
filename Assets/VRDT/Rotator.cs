13323f89ed3cb2e09ea83c674d2f53182fc276e6
Kris Schultz <kschultz@resourceammirati.com>
2018-05-13 12:51:46 -0400

    Added caustics effect.

---
+++ Assets/VRDT/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+
+    public float rollSpeed = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        gameObject.transform.Rotate(Vector3.forward * rollSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/VRDT/VideoLightCookie.cs
@@ -0,0 +1,24 @@
+﻿/**
+ * Allows you to use a video as a cookie on a projector.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoLightCookie : MonoBehaviour {
+
+
+    public MovieTexture movie;
+
+    private Projector projector;
+
+	// Use this for initialization
+	void Start () {
+        projector = GetComponent<Projector>();
+        projector.material.SetTexture("_ShadowTex", movie);
+        movie.loop = true;
+        movie.Play();
+	}
+	
+}
